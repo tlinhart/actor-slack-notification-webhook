4ee425962bfeb880e9ae7dcbd760c165cda0dd97
Tomáš Linhart <pasmen@gmail.com>
2021-10-05 12:43:42 +0200

    Refactor utility functions to standalone module

--- src/main.js
+++ src/main.js
@@ -3,32 +3,8 @@ const { IncomingWebhook } = require("@slack/webhook");
 const { readFileSync } = require("fs");
 const { defaults, mapKeys, snakeCase } = require("lodash");
 const { mapValuesDeep } = require("deepdash/standalone");
+const { interpolate } = require("./utils");
 const { log } = Apify.utils;
-
-/**
- * Interpolate the string using given parameters.
- *
- * The string is interpolated as if it was a template literal but with only
- * the parameters available. Nested interpolation is also supported.
- */
-const interpolate = (string, params) => {
-  const keys = Object.keys(params);
-  const values = Object.values(params);
-
-  const _interpolate = (s) => {
-    const escaped = s.replace(/`/g, "\\`");
-    return new Function(...keys, `return \`${escaped}\`;`)(...values);
-  };
-
-  // Use multiple passes if needed to support nested interpolation.
-  let input;
-  let result = string;
-  do {
-    input = result;
-    result = _interpolate(input);
-  } while (result !== input);
-  return result;
-};
 
 Apify.main(async () => {
   const {
---
+++ src/utils.js
@@ -0,0 +1,24 @@
+/**
+ * Interpolate the string using given parameters.
+ *
+ * The string is interpolated as if it was a template literal but with only
+ * the parameters available. Nested interpolation is also supported.
+ */
+module.exports.interpolate = (string, params) => {
+  const keys = Object.keys(params);
+  const values = Object.values(params);
+
+  const _interpolate = (s) => {
+    const escaped = s.replace(/`/g, "\\`");
+    return new Function(...keys, `return \`${escaped}\`;`)(...values);
+  };
+
+  // Use multiple passes if needed to support nested interpolation.
+  let input;
+  let result = string;
+  do {
+    input = result;
+    result = _interpolate(input);
+  } while (result !== input);
+  return result;
+};
